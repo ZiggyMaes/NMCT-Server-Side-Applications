d72e46e63c29acd4432ba8edddc04b489dae51b1
Ziggy Maes <ziggy.maes@student.howest.be>
2015-09-25 12:01:27 +0200

    Oefening 4...

---
+++ Week 1/01-Intro/Controllers/ServerVariablesController.cs
@@ -0,0 +1,29 @@
+﻿using _01_Intro.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace _01_Intro.Controllers
+{
+    public class ServerVariablesController : Controller
+    {
+        // GET: ServerVariables
+        public ActionResult ServerVariables()
+        {
+            List<ServerVariable> vars = new List<ServerVariable>();
+            foreach (var key in Request.ServerVariables.AllKeys)
+            {
+                vars.Add(new ServerVariable()
+                {
+                    Key = key.ToString(),
+                    Value = Request.ServerVariables[key.ToString()]
+                });
+            }
+
+            ViewBag.Result = vars;
+            return View();
+        }
+    }
+}
