c8442950f730214899ae041529cc33d627e74dfc
Ziggy Maes <ziggy.maes@student.howest.be>
2015-09-25 12:26:02 +0200

    Oefening 5...

--- Week 1/01-Intro/App_Start/RouteConfig.cs
+++ Week 1/01-Intro/App_Start/RouteConfig.cs
@@ -16,7 +16,7 @@ namespace _01_Intro
             routes.MapRoute(
                 name: "Default",
                 url: "{controller}/{action}/{id}",
-                defaults: new { controller = "Time", action = "WhatTime", id = UrlParameter.Optional }
+                defaults: new { controller = "Contact", action = "Index", id = UrlParameter.Optional }
             );
         }
     }
---
+++ Week 1/01-Intro/Controllers/ContactController.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace _01_Intro.Controllers
+{
+    public class ContactController : Controller
+    {
+        // GET: Contact
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult Save(string title, string info)
+        {
+            if (String.IsNullOrEmpty(title) || String.IsNullOrEmpty(info)) return View("Error");
+            ViewBag.Title = title;
+            ViewBag.Info = info;
+            return View();
+        }
+    }
+}
