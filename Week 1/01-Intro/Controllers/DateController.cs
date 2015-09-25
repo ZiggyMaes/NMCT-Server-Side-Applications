b2ef4f3071dc392383b91c2e18f44a19258caedb
Ziggy Maes <ziggy.maes@student.howest.be>
2015-09-25 11:05:30 +0200

    Oefening 2...

---
+++ Week 1/01-Intro/Controllers/DateController.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace _01_Intro.Controllers
+{
+    public class DateController : Controller
+    {
+        //// GET: Date
+        //public ActionResult Index()
+        //{
+        //    return View();
+        //}
+
+        public ActionResult Today()
+        {
+            ViewBag.Today = DateTime.Now.ToString();
+            //return null;
+             return View();
+        }
+
+        public ActionResult Tomorrow()
+        {
+            ViewBag.Tomorrow = DateTime.Now.AddDays(1).ToString();
+            return View("Today");
+        }
+
+        public ActionResult Yesterday()
+        {
+            ViewBag.Tomorrow = DateTime.Now.AddDays(-1).ToString();
+            return View("Today");
+        }
+    }
+}
---
+++ Week 1/01-Intro/Controllers/TimeController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace _01_Intro.Controllers
+{
+    public class TimeController : Controller
+    {
+        // GET: Time
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
