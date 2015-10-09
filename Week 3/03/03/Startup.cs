4ba9cbaf96e05f3deb9dba24697de067f260a14e
Ziggy Maes <ziggy.maes@student.howest.be>
2015-10-09 09:39:13 +0200

    Week 3 stage files + new class Session

---
+++ Week 3/03/03/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(_03.Startup))]
+namespace _03
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
