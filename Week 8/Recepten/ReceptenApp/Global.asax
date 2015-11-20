9cb7f3d2b5ae61e2d1a5597f15bd0b79d026a850
Ziggy Maes <ziggy.maes@student.howest.be>
2015-11-20 09:50:28 +0100

    VS Stage files

---
+++ Week 8/Recepten/ReceptenApp/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="ReceptenApp.MvcApplication" Language="C#" %>
---
+++ Week 8/Recepten/ReceptenApp/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(ReceptenApp.Startup))]
+namespace ReceptenApp
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
