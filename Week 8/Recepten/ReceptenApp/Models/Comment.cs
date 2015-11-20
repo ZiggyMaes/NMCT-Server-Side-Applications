59402ce4aa3fc61387e5f0ea679a86bb377e0813
Ziggy Maes <ziggy.maes@student.howest.be>
2015-11-20 11:59:29 +0100

    Model creation

---
+++ Week 8/Recepten/ReceptenApp/Models/Comment.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ReceptenApp.Models
+{
+    public class Comment
+    {
+        public int Id { get; set; }
+        public int RecipeId { get; set; }
+        public string Description { get; set; }
+        public int UserId { get; set; }
+        public DateTime TimeStamp { get; set; }
+    }
+}
---
+++ Week 8/Recepten/ReceptenApp/Models/Recipe.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ReceptenApp.Models
+{
+    public class Recipe
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Description { get; set; }
+        public int CategoryId { get; set; }
+        public int UserId { get; set; }
+
+    }
+}
