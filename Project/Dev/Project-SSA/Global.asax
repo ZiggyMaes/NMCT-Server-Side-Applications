3d1d72f84c067b3ab5e389361bbd25ea4cb4a19e
Ziggy Maes <ziggy.maes@student.howest.be>
2015-12-03 11:55:44 +0100

    VS project initialization

---
+++ Project/Dev/Project-SSA/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="Project_SSA.MvcApplication" Language="C#" %>
---
+++ Project/Dev/Project-SSA/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(Project_SSA.Startup))]
+namespace Project_SSA
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
