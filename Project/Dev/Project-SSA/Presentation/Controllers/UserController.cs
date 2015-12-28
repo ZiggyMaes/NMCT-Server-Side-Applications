925c0959ebaa0e62ce365b550439b442c010ce92
Ziggy Maes <ziggy.maes@student.howest.be>
2015-12-28 18:09:14 +0100

    User models and controller

---
+++ Project/Dev/Project-SSA/Presentation/Controllers/UserController.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Presentation.Controllers
+{
+    public class UserController : Controller
+    {
+        // GET: User
+        public ActionResult Login()
+        {
+            return View();
+        }
+        public ActionResult Register()
+        {
+            return View();
+        }
+
+
+    }
+}
