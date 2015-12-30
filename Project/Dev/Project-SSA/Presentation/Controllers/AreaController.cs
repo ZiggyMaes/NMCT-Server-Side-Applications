fb5a06280a0a41530e4e7a8e257015820e679371
Ziggy Maes <ziggy.maes@student.howest.be>
2015-12-30 16:22:27 +0100

    Working area repository and controller

---
+++ Project/Dev/Project-SSA/Presentation/Controllers/AreaController.cs
@@ -0,0 +1,23 @@
+﻿using Presentation.Models;
+using Presentation.Repositories;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Presentation.Controllers
+{
+    public class AreaController : Controller
+    {
+        // GET: Area
+        public ActionResult Index(int? AreaId)
+        {
+            if (AreaId == null) return RedirectToAction("Index", "Home");
+
+            Area a = AreaRepository.GetAreaInfo(Convert.ToInt32(AreaId));
+
+            return View(a);
+        }
+    }
+}
---
+++ Project/Dev/Project-SSA/Presentation/Repositories/AreaRepository.cs
@@ -0,0 +1,42 @@
+﻿using Presentation.Models;
+using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Presentation.Repositories
+{
+    public class AreaRepository
+    {
+        private string CONNECTIONSTRING = ConfigurationManager.ConnectionStrings["DefaultConnection"].ConnectionString;
+
+        public static Area GetAreaInfo(int AreaId)
+        {
+            Area area = new Area();
+
+            using (SqlConnection con = new SqlConnection(ConfigurationManager.ConnectionStrings["DefaultConnection"].ConnectionString))
+            {
+                con.Open();
+
+                using (SqlCommand cmd = new SqlCommand())
+                {
+                    cmd.Connection = con;
+                    cmd.CommandText = "SELECT Id, Title, Description FROM dbo.Area WHERE Id = @AreaId";
+                    cmd.Parameters.AddWithValue("AreaId", AreaId);
+
+                    SqlDataReader reader = cmd.ExecuteReader();
+                    while (reader.Read())
+                    {
+                        area.Id = int.Parse(reader["Id"].ToString());
+                        area.Title = (reader["Title"] == DBNull.Value ? string.Empty : reader["Title"].ToString());
+                        area.Description = (reader["Description"] == DBNull.Value ? string.Empty : reader["Description"].ToString());
+                    }
+                }
+            }
+            return area;
+        }
+    }
+}
