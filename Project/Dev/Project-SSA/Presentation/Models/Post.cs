ffcf8017f5b41fb4856e512e1cbb35001bea6feb
Ziggy Maes <ziggy.maes@student.howest.be>
2015-12-30 15:31:24 +0100

    Connectionstring password fix + created forum model + added forum view

---
+++ Project/Dev/Project-SSA/Presentation/Models/Post.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Presentation.Models
+{
+    public class Post
+    {
+        public int Id { get; set; }
+        public string Title { get; set; }
+        public string Body { get; set; }
+        public DateTime TimePosted { get; set; }
+        public int ParentId { get; set; }
+        public bool Visible { get; set; }
+        public int AreaId { get; set; }
+        public int UserId { get; set; }
+
+    }
+}
