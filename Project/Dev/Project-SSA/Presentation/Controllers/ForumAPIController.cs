1c1466620ccbc7f3d5769fd4a8076474d628c35d
Ziggy Maes <ziggy.maes@student.howest.be>
2016-01-05 02:09:09 +0100

    API

---
+++ Project/Dev/Project-SSA/Presentation/Controllers/ForumAPIController.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Http;
+using Presentation.Models;
+using Presentation.Repositories;
+
+namespace Presentation.Controllers
+{
+    public class ForumAPIController : ApiController
+    {
+
+        [HttpGet]
+        public IHttpActionResult GetMessage(int MessageId)
+        {
+            Message FetchedMessaged = ForumRepository.GetMessage(MessageId);
+
+            if (FetchedMessaged == null)  return NotFound();
+            else return Ok(FetchedMessaged);
+        }
+
+        [HttpGet]
+        public IEnumerable<Message> GetThreadContent(int AreaId)
+        {
+            return ForumRepository.GetThreads(AreaId);
+        }
+    }
+}
--- Project/Dev/Project-SSA/Presentation/Global.asax.cs
+++ Project/Dev/Project-SSA/Presentation/Global.asax.cs
@@ -1,8 +1,10 @@
-﻿using System;
+﻿using Presentation.App_Start;
+using System;
 using System.Collections.Generic;
 using System.Data.Entity;
 using System.Linq;
 using System.Web;
+using System.Web.Http;
 using System.Web.Mvc;
 using System.Web.Optimization;
 using System.Web.Routing;
@@ -14,6 +16,7 @@ namespace Presentation
         protected void Application_Start()
         {
             AreaRegistration.RegisterAllAreas();
+            GlobalConfiguration.Configure(WebApiConfig.Register);
             FilterConfig.RegisterGlobalFilters(GlobalFilters.Filters);
             RouteConfig.RegisterRoutes(RouteTable.Routes);
             BundleConfig.RegisterBundles(BundleTable.Bundles);     
